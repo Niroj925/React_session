e32c6b4d144c6af759b779cb73b590b105292077
Niroj925 <ngtc0925@gmail.com>
2022-06-24 07:07:49 +0545

    react18

--- optimization/src/index.js
+++ optimization/src/index.js
@@ -1,4 +1,7 @@
-import {render }from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-render(<App/>, document.getElementById('root'));
+//render(<App/>, document.getElementById('root'));
+const root=ReactDOM.createRoot(document.getElementById('root'));//this syntax is for react 18
+
+root.render(<App/>);
