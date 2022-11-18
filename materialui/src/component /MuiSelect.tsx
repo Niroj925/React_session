77c2f9dcbb7c58962fcba7a9347f5e622cb5f41b
Niroj925 <ngtc0925@gmail.com>
2022-11-18 19:25:08 +0545

    multiselect properties

--- materialui/src/component /MuiSelect.tsx
+++ materialui/src/component /MuiSelect.tsx
@@ -3,18 +3,22 @@ import {Box,TextField,MenuItem} from '@mui/material';
 import {useState } from 'react';
 
 function MuiSelect() {
-    const [country ,setCountry]=useState('')
-      console.log({country})
+    const [countries ,setCountries]=useState<string[]>([])
+      console.log({countries})
     const handleChange=(event:React.ChangeEvent<HTMLInputElement>)=>{
-        setCountry(event.target.value as string)
+        const value=event.target.value
+        setCountries(typeof value==='string'?value.split(','):value)
     }
   return (
     <Box width='250px'>
-      <TextField label='select country' 
+      <TextField label='select countries' 
       select
-       value={country}
+       value={countries}
       onChange={handleChange}
       fullWidth
+      SelectProps={{
+        multiple:true
+      }}
       >
       <MenuItem value='NP'>Nepal</MenuItem>
       <MenuItem value='CH'>China</MenuItem>
