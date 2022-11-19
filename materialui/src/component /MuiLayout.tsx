bac85826c5d560b891f07f5c24146fecc739aacb
Niroj925 <ngtc0925@gmail.com>
2022-11-19 18:55:07 +0545

    grid

--- materialui/src/component /MuiLayout.tsx
+++ materialui/src/component /MuiLayout.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Box,Stack,Divider} from '@mui/material'
+import {Box,Stack,Divider,Grid} from '@mui/material'
 
 function MuiLayout() {
   return (
     // stack is a one dimentional either vertical or horizontal
     // spacing 1=8px
+    <>
     <Stack 
     sx={{border:'1px solid'}}
      direction='row' 
@@ -34,6 +35,26 @@ function MuiLayout() {
    >
    </Box>
    </Stack>
+   {/* grid is two dimensional  */}
+   {/* my is margin and p is padding 1=8px */}
+   <Grid container my={2} rowSpacing={2} columnSpacing={1}>
+    {/* xs ={9 } mean 9 out of 12 column grid layout  for web and sm={12} for mobile*/}
+    {/* xs extra small for mobiel view  and sm for table and large */}
+    <Grid item xs={12} sm={6}>
+        <Box bgcolor='success.light' p={2}>item1 xs12 sm4 col</Box>
+    </Grid>
+    <Grid item xs={12} sm={6}>
+        <Box bgcolor='primary.light' p={2} >item1 xs12 sm4 col</Box>
+    </Grid>
+    <Grid item xs={12} sm={6}>
+        <Box bgcolor='success.light' p={2}>item1 xs12 sm4 col</Box>
+    </Grid>
+    <Grid item xs={12} sm={6}>
+        <Box bgcolor='success.light' p={2}>item1 xs12 sm4 col</Box>
+    </Grid>
+
+   </Grid>
+   </>
   )
 }
 
