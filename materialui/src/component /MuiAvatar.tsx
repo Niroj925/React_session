40e8037b93349dea6174862fbab43fbae95c3659
Niroj925 <ngtc0925@gmail.com>
2022-11-21 17:26:30 +0545

    avatar group height width variant

--- materialui/src/component /MuiAvatar.tsx
+++ materialui/src/component /MuiAvatar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Stack,Avatar} from '@mui/material'
+import {Stack,Avatar,AvatarGroup} from '@mui/material'
 
 function MuiAvatar() {
   return (
@@ -9,8 +9,12 @@ function MuiAvatar() {
            <Avatar sx={{bgcolor:'success.light'}}>BW</Avatar>
         </Stack>       
         <Stack spacing={2} direction="row">
+            <AvatarGroup max={4}>
+            <Avatar variant='square' sx={{bgcolor:'primary.light',width:70,height:70 }}>CM</Avatar>
+           <Avatar variant='rounded' sx={{bgcolor:'success.light'}}>BW</Avatar>
          <Avatar 
          src='https://randomuser.me/api/portraits/women/55.jpg'
+         sx={{width:70,height:70 }}
           alt='dear'/>
            <Avatar 
          src='https://randomuser.me/api/portraits/women/69.jpg'
@@ -18,6 +22,7 @@ function MuiAvatar() {
            <Avatar 
          src='https://randomuser.me/api/portraits/women/36.jpg'
           alt='dear'/>
+          </AvatarGroup>
         </Stack>       
     </Stack>
   )
